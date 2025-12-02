391026543f0b7aa0f4a0ca344a28b556e8b57cff
pmt1841 <phamminhtien1841@gmail.com>
2025-12-02 11:35:36 +0700

    Create PhoneNumberTest.java

---
+++ src/PhoneNumberTest.java
@@ -0,0 +1,17 @@
+public class PhoneNumberTest {
+    public static void main(String[] args) {
+        String[] phoneNumberList = {
+                "(84)-(0978489648)",
+                "(a8)-(22222222)",
+                "(84)-(22b22222)",
+                "(84)-(9978489648)"
+        };
+
+        PhoneNumber phoneNumber = new PhoneNumber();
+        
+        for (String number : phoneNumberList) {
+            boolean validated = phoneNumber.validate(number);
+            System.out.println("Phone number: " + number + " is valid: " + validated);
+        }
+    }
+}
