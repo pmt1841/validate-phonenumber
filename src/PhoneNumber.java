a114e1bba8520aa6cfd61565b41fed85c14f9e01
pmt1841 <phamminhtien1841@gmail.com>
2025-12-02 15:53:54 +0700

    Update PhoneNumber.java

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumber {
-    private static final String PHONENUMBER_REGEX = "^[(]\\d{2}[)]-[(]0\\d{9}[)]$";
+    private static final String PHONENUMBER_REGEX = "^([(]\\d{2}[)])-([(]0\\d{9}[)])$";
 
     public PhoneNumber() {
     }
